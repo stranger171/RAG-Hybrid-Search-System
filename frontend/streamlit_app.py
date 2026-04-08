60ddf9d876913e5274be6b7a8851ad89744f975e
Shashank singh <shashankth171@gmail.com>
2026-04-08 16:17:53 +0530

    streamlit

---
+++ frontend/streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="AI Chatbot", layout="wide")
+
+st.title("🤖 Student AI Chatbot")
+
+# chat history store
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# show old messages
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+# input box
+if prompt := st.chat_input("Ask something..."):
+
+    # show user message
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # call backend API
+    try:
+        response = requests.post(
+            "http://127.0.0.1:8000/chat/",
+            json={"query": prompt}
+        )
+
+        answer = response.json().get("answer", "No response")
+
+    except:
+        answer = "❌ Backend not running"
+
+    # show bot response
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    with st.chat_message("assistant"):
+        st.markdown(answer)
